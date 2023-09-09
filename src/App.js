710f35ade608c1605bb3d75ce2934faff7dc0e6e
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2023-09-09 23:45:57 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css'; // Import your custom CSS file
 import VideoPlayer from './components/VideoPlayer';
 import Notifications from './components/Notifications';
 import Sidebar from './components/Options';
+import { SocketContext } from './SocketContext';
+import { useContext } from 'react';
 
 const App = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
   return (
     <div className="wrapper"> 
       <div className="app-bar">
         <h2 className="app-title">Video Chat</h2>
+        <h5>{name || 'Name'}</h5>
       </div>
       <VideoPlayer />
       <div className="custom-dropdown">
